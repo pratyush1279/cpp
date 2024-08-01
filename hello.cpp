364b3ab762b2b1bcf38924bc80b664c1558f4af5
pratyush1279 <pratyushpatwal@gmail.com>
2024-08-01 17:40:29 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+// C++ program to illustrate for loop to print numbers from 
+// 1 to n 
+
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+	// initializing n (value upto which you want to print 
+	// numbers 
+	int n = 5; 
+	int i; // initialization of loop variable 
+	for (i = 1; i <= n; i++) { 
+		cout << i << " "; 
+	} 
+
+	return 0; 
+}
---
+++ hi.cpp
@@ -0,0 +1,6 @@
+#include<iostream> // Include the iostream library for input and output
+
+int main() { // Main function where the execution of the program begins
+    std::cout << "Hi my name is priya ka pati" << std::endl; // Output "Hello, World!" to the console
+    return 0; // Return 0 to indicate successful execution
+}
